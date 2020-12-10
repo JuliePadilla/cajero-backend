aa186399fd30ac59f82b2ca1f9189344061e5fb1
Julie Padilla <julie.andrea.padilla@gmail.com>
2020-12-10 08:21:32 -0500

    Primera versión funcional del backend

---
+++ models/transaction_models.py
@@ -0,0 +1,14 @@
+from typing import Optional
+from pydantic import BaseModel
+from datetime import datetime
+
+class TransactionIn(BaseModel):
+    username: str
+    value: int
+
+class TransactionOut(BaseModel):
+    id_transaction: int
+    username: str
+    date: datetime
+    value: int
+    actual_balance: int
